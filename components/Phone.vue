b3d98f2ff5cd2a36f61ccba806527e2754e1c4fa
Tigran <harutyunyan.tigran1975@gmail.com>
2024-05-13 08:20:30 +0400

    created Phone component to show in designer page

---
+++ components/Phone.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+interface PhoneProps {
+  imgSrc: string;
+  dark?: boolean;
+}
+
+const { imgSrc, dark } = defineProps<PhoneProps>();
+</script>
+
+<template>
+  <div class="relative pointer-events-none z-50 overflow-hidden">
+    <img
+      :src="
+        dark
+          ? '/phone-template-dark-edges.png'
+          : '/phone-template-white-edges.png'
+      "
+      class="pointer-events-none z-50 select-none"
+      alt="phone image"
+    />
+
+    <div class="absolute -z-10 inset-0">
+      <img
+        class="object-cover min-w-full min-h-full"
+        :src="imgSrc"
+        alt="overlaying phone image"
+      />
+    </div>
+  </div>
+</template>
