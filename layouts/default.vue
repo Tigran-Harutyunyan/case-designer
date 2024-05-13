a30d8e1889ea40c002501b62ae81ee7130a96f46
Tigran <harutyunyan.tigran1975@gmail.com>
2024-05-13 08:18:03 +0400

    created default and auth layouts

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <Navbar />
+  <main class="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
+    <div class="flex-1 flex flex-col h-full">
+      <slot />
+    </div>
+    <Footer />
+  </main>
+</template>
