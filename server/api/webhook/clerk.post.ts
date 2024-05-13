47835d310376a1f49ad6379e6029e692466bc621
Tigran <harutyunyan.tigran1975@gmail.com>
2024-05-13 21:17:35 +0400

    commited incompleted clerk webhook for testing purposes

---
+++ server/api/webhook/clerk.post.ts
@@ -0,0 +1,51 @@
+import { clerkClient } from 'h3-clerk';
+import { db } from '@/lib/prismadb';
+
+export default defineEventHandler(async (event) => {
+
+    const { data, type: eventType } = await readBody(event);
+
+    const { auth } = event.context;
+
+    if (!(auth?.userId)) {
+        return createError({
+            statusCode: 500,
+            statusMessage: 'You need to be logged in to view this page.'
+        })
+    }
+
+    const user = await clerkClient.users.getUser(auth?.userId);
+
+    try {
+
+        if (eventType === "user.created") {
+            const { id, email_addresses, image_url, first_name, last_name, username } = data;
+
+            if (!user?.id) {
+                throw new Error('Invalid user data')
+            }
+
+            const existingUser = await db.user.findFirst({
+                where: { id: user.id },
+            })
+
+            if (!existingUser) {
+                await db.user.create({
+                    data: {
+                        id: user.id,
+                        email: user.email,
+                    },
+                })
+            }
+
+            return { success: true }
+        }
+
+
+    } catch (error) {
+        return createError({
+            statusCode: 500,
+            statusMessage: error?.message
+        })
+    }
+});
