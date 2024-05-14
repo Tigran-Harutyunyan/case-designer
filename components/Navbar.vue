fe93bc90d1453e5574635884cc8bdca7844f4066
Tigran <harutyunyan.tigran1975@gmail.com>
2024-05-14 19:23:26 +0400

    refactoreed Navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,11 +3,10 @@ import { buttonVariants } from "./ui/button";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-vue-next";
 
-import { useClerkProvide, ClerkLoaded, SignOutButton } from "vue-clerk";
+import { useClerkProvide, SignOutButton } from "vue-clerk";
 const { state } = useClerkProvide();
 
-const isAdmin = true;
-const user = null;
+const isAdmin = await $fetch("/api/admin");
 </script>
 
 <template>
@@ -18,99 +17,97 @@ const user = null;
       <div
         class="flex h-14 items-center justify-between border-b border-zinc-200"
       >
-        <ClerkLoaded>
-          <NuxtLink to="/" class="flex z-40 font-semibold">
-            case<span class="text-green-600">cobra</span>
-          </NuxtLink>
+        <NuxtLink to="/" class="flex z-40 font-semibold">
+          case<span class="text-green-600">cobra</span>
+        </NuxtLink>
 
-          <div class="h-full flex items-center space-x-4">
-            <template v-if="state.user">
-              <SignOutButton v-slot="{ handler }">
-                <button
-                  class="cursor-pointer"
-                  :class="
-                    buttonVariants({
-                      size: 'sm',
-                      variant: 'ghost',
-                    })
-                  "
-                  @click="handler"
-                >
-                  Sign out
-                </button>
-              </SignOutButton>
-
-              <NuxtLink
-                v-if="isAdmin"
-                to="/dashboard"
+        <div class="h-full flex items-center space-x-4">
+          <template v-if="state.user">
+            <SignOutButton v-slot="{ handler }">
+              <button
+                class="cursor-pointer"
                 :class="
                   buttonVariants({
                     size: 'sm',
                     variant: 'ghost',
                   })
                 "
+                @click="handler"
               >
-                Dashboard ✨
-              </NuxtLink>
+                Sign out
+              </button>
+            </SignOutButton>
 
-              <NuxtLink
-                to="/configure/upload"
-                :class="
-                  buttonVariants({
-                    size: 'sm',
-                    class: 'hidden sm:flex items-center gap-1',
-                  })
-                "
-              >
-                Create case
-                <ArrowRight class="ml-1.5 h-5 w-5" />
-              </NuxtLink>
-            </template>
+            <NuxtLink
+              v-if="isAdmin"
+              to="/dashboard"
+              :class="
+                buttonVariants({
+                  size: 'sm',
+                  variant: 'ghost',
+                })
+              "
+            >
+              Dashboard ✨
+            </NuxtLink>
 
-            <template v-else>
-              <NuxtLink
-                to="/sign-in"
-                external
-                :class="
-                  buttonVariants({
-                    size: 'sm',
-                    variant: 'ghost',
-                  })
-                "
-              >
-                Sign In
-              </NuxtLink>
+            <NuxtLink
+              to="/configure/upload"
+              :class="
+                buttonVariants({
+                  size: 'sm',
+                  class: 'hidden sm:flex items-center gap-1',
+                })
+              "
+            >
+              Create case
+              <ArrowRight class="ml-1.5 h-5 w-5" />
+            </NuxtLink>
+          </template>
 
-              <NuxtLink
-                to="/sign-up"
-                external
-                :class="
-                  buttonVariants({
-                    size: 'sm',
-                    variant: 'ghost',
-                  })
-                "
-              >
-                Sign Up
-              </NuxtLink>
+          <template v-else>
+            <NuxtLink
+              to="/sign-in"
+              external
+              :class="
+                buttonVariants({
+                  size: 'sm',
+                  variant: 'ghost',
+                })
+              "
+            >
+              Sign In
+            </NuxtLink>
 
-              <div class="h-8 w-px bg-zinc-200 hidden sm:block" />
+            <NuxtLink
+              to="/sign-up"
+              external
+              :class="
+                buttonVariants({
+                  size: 'sm',
+                  variant: 'ghost',
+                })
+              "
+            >
+              Sign Up
+            </NuxtLink>
 
-              <NuxtLink
-                to="/configure/upload"
-                :class="
-                  buttonVariants({
-                    size: 'sm',
-                    class: 'hidden sm:flex items-center gap-1',
-                  })
-                "
-              >
-                Create case
-                <ArrowRight class="ml-1.5 h-5 w-5" />
-              </NuxtLink>
-            </template>
-          </div>
-        </ClerkLoaded>
+            <div class="h-8 w-px bg-zinc-200 hidden sm:block" />
+
+            <NuxtLink
+              to="/configure/upload"
+              :class="
+                buttonVariants({
+                  size: 'sm',
+                  class: 'hidden sm:flex items-center gap-1',
+                })
+              "
+            >
+              Create case
+              <ArrowRight class="ml-1.5 h-5 w-5" />
+            </NuxtLink>
+          </template>
+        </div>
       </div>
     </MaxWidthWrapper>
   </nav>
